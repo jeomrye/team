86fe2c24ac072e7a513fe4edac7a471d518fa48b
admin <admin@DESKTOP-KV3M1GF>
2021-01-21 16:26:31 +0900

    commiut

--- StudyRoom/src/main/java/com/std/domain/MemVO.java
+++ StudyRoom/src/main/java/com/std/domain/MemVO.java
@@ -10,7 +10,6 @@ import lombok.Data;
 public class MemVO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	private Long uno;	//회원순번
 	private String userid; //회원id
 	private int memberno; //회원번호
 	private String username; //회원이름
@@ -20,9 +19,9 @@ public class MemVO implements Serializable {
 	private String gender; //성별
 	private Date regdate; //회원가입일
 	private Date updatedate; //회원정보 수정일
-	private int mileage;	//마일리지
-	private String coupon; //쿠폰번호
+	private int mileage;
 	private int companynumber; //사업자번호
+	
 	private List<AuthVO> authList;		
-
+	
 }
